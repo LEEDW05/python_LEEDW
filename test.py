26e852f718f6a35f84f44a7a087d1e9fc07f7e2a
LEEDW <minamwon>
2025-01-15 10:13:50 +0900

    종료삽입

---
+++ test.py
@@ -0,0 +1,11 @@
+def rectArea(width,height):
+    return width * height
+
+def triArea(width,height):
+    return (width * height)/2
+
+
+if __name__ == '__main__':
+    print(f'사각형 넓이 = {rect(10,5)}')
+    print(f'삼각형 넓이 = {rect(10,5)}')
+    print('종료')
